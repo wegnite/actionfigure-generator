12065c3ea418aec91c1d1d306bf1d491ba01d924
Drew <9148877+mryangsd@user.noreply.gitee.com>
2025-08-30 11:21:40 +0800

    feat:支持 cf 部署

---
+++ src/app/[locale]/pay-success/[session_id]/page.tsx
@@ -0,0 +1,25 @@
+import Stripe from "stripe";
+import { handleOrderSession } from "@/services/order";
+import { redirect } from "next/navigation";
+
+export default async function ({
+  params,
+}: {
+  params: Promise<{ session_id: string }>;
+}) {
+  try {
+    const { session_id } = await params;
+
+    const stripe = new Stripe(process.env.STRIPE_PRIVATE_KEY || "", {
+      // Cloudflare Workers use the Fetch API for their API requests.
+      httpClient: Stripe.createFetchHttpClient(),
+    });
+    const session = await stripe.checkout.sessions.retrieve(session_id);
+
+    await handleOrderSession(session);
+  } catch (e) {
+    redirect(process.env.NEXT_PUBLIC_PAY_FAIL_URL || "/");
+  }
+
+  redirect(process.env.NEXT_PUBLIC_PAY_SUCCESS_URL || "/");
+}
